3c23e25186245b2cfdcfc1f02db8f954768baf83
Roberto Romero <robextremo@gmail.com>
2025-04-16 14:49:42 -0600

    optimizacion de imagenes

---
+++ js/hello.js
@@ -0,0 +1,14 @@
+  window.onload = function () {
+    const container = document.getElementById("saludo");
+    const spans = container.querySelectorAll("span");
+    let index = 0;
+
+    function mostrarSiguienteSaludo() {
+      spans.forEach(span => span.classList.remove("visible"));
+      spans[index].classList.add("visible");
+      index = (index + 1) % spans.length;
+    }
+
+    mostrarSiguienteSaludo(); // Mostrar el primero
+    setInterval(mostrarSiguienteSaludo, 340); // Cambiar cada segundo
+  };
