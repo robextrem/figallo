b3de5360f5204f04452b036473ae9c2f1cbe6bac
Roberto Romero <robextremo@gmail.com>
2025-04-18 14:16:12 -0600

    htaccess

--- js/hello.js
+++ js/hello.js
@@ -1,14 +1,1 @@
-  window.onload = function () {
-	const container = document.getElementById("saludo");
-	const spans = container.querySelectorAll("span");
-	let index = 0;
-
-	function mostrarSiguienteSaludo() {
-		spans.forEach(span => span.classList.remove("visible"));
-		spans[index].classList.add("visible");
-		index = (index + 1) % spans.length;
-	}
-
-	mostrarSiguienteSaludo(); 
-	setInterval(mostrarSiguienteSaludo, 340);
-  };
+window.onload=function(){const e=document.getElementById("saludo").querySelectorAll("span");let l=0;function t(){e.forEach((e=>e.classList.remove("visible"))),e[l].classList.add("visible"),l=(l+1)%e.length}t(),setInterval(t,340)};
