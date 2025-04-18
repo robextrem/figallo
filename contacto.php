ea25cdbdc72609d365ca1a90167d100c330f8893
Roberto Romero <robextremo@gmail.com>
2025-04-18 14:23:43 -0600

    htaccess

--- contacto.php
+++ contacto.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $headers .= "Reply-To: $correo\r\n";
     if (mail($destinatario, $asunto, $mensaje, $headers)) {
         // Redirigir a una página de agradecimiento
-        header('Location: thankyou-page.html');
+        header('Location: /thankyou');
         exit; // Para asegurarnos de que el script se detenga después de la redirección
     } else {
         // Mostrar un mensaje de error si no se pudo enviar el correo electrónico
